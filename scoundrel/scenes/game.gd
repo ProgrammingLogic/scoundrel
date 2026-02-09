7a0e05d013bcdbba0e2bbd5e36c209358881c4a6
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2026-02-09 14:22:18 -0600

    debug: try to debug the stupid card placement

--- scoundrel/scenes/game.gd
+++ scoundrel/scenes/game.gd
@@ -14,8 +14,8 @@ func _ready() -> void:
 
 func start_game() -> void:
 	create_deck()
-
-	draw_room()
+	#deck.shuffle()
+	#draw_room()
 
 
 ## Create the deck that is to be used for the game.
@@ -31,13 +31,36 @@ func create_deck() -> void:
 
 	for suite in suites:
 		for value in values:
+			var card_name = "%s_%s" % [suite, value]
+
 			# Remove diamond/heart A, J, Q, K from the deck
-			if suite in ["diamond", "heart"] and value in ["A", "J", "Q", "K"]:
+			if suite in ["diamonds", "hearts"] and value in ["A", "J", "Q", "K"]:
+				var card = card_manager.card_factory.create_card(card_name, discard)
+				discard.add_card(card) # Testing
 				continue
 
-			var card_name = "%s_%s" % [suite, value]
+			if value in ["2", "3", "4", "5"] and suite in ["spades"]:
+				var card = card_manager.card_factory.create_card(card_name, room)
+				room.add_card(card) # Testing
+				continue
+
 			var card = card_manager.card_factory.create_card(card_name, deck)
 			deck.add_card(card)
+
+	assert(deck.get_card_count() > 0)
+	for card in deck.get_top_cards(deck.get_card_count()):
+		assert(not(
+			card.card_info["suit"] in ["heart", "diamond"] and
+			card.card_info["value"] in ["A", "J", "Q", "K"]
+		))
+
+	assert(discard.get_card_count() > 0)
+	for card in discard.get_top_cards(discard.get_card_count()):
+		assert(
+			card.card_info["suit"] in ["heart", "diamond"] and
+			card.card_info["value"] in ["A", "J", "Q", "K"]
+		)
+
 
 
 ## Draw cards into the room.
