a32d45da0dc33c8a5e0479a7cdba4f597ca9f8b9
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2026-02-21 09:16:39 -0600

    feature: polish layout of cards

--- globals.gd
+++ globals.gd
@@ -6,6 +6,8 @@ var game: Control
 var room: CardHand
 var deck: CardPile
 var selected_indicator: SelectedIndicator
+var card_size: Vector2 = Vector2()
+var theme: Theme = preload("res://resources/game_theme.tres")
 
 enum Cards {
 	ACE = 14,
--- scenes/card_layouts/card_front.gd
+++ scenes/card_layouts/card_front.gd
@@ -1,10 +1,5 @@
 class_name CardFrontLayout extends CardLayout
 
-@onready var card_background: Panel = %CardBackground
-@onready var card_value: Label = %CardValue
-@onready var card_suite: Label = %CardSuite
-
-
 func _update_display() -> void:
 	var data = card_resource as ScoundrelCardResource
 	assert(data)
@@ -13,31 +8,31 @@ func _update_display() -> void:
 	assert(data.value < 15)
 	assert(data.card_type in ["monster", "potion", "weapon"])
 
-	match data.suite:
-		"hearts":
-			card_value.modulate = Color.RED
-			card_suite.text = "♥️"
-		"diamonds":
-			card_value.modulate = Color.RED
-			card_suite.text = "♦️"
-		"clubs":
-			card_value.modulate = Color.BLACK
-			card_suite.text = "♣️"
-		"spades":
-			card_value.modulate = Color.BLACK
-			card_suite.text = "♠️"
+	match data.card_type:
+		"monster":
+			%CardValue.theme_type_variation = "MonsterCard"
+			%CardType.theme_type_variation = "MonsterCard"
+			%CardType.text = "Monster"
+		"weapon":
+			%CardValue.theme_type_variation = "WeaponCard"
+			%CardType.theme_type_variation = "WeaponCard"
+			%CardType.text = "Weapon"
+		"potion":
+			%CardValue.theme_type_variation = "PotionCard"
+			%CardType.theme_type_variation = "PotionCard"
+			%CardType.text = "Potion"
 
 	match data.value:
 		14:
-			card_value.text = "A"
+			%CardValue.text = "A"
 		13:
-			card_value.text = "K"
+			%CardValue.text = "K"
 		12:
-			card_value.text = "Q"
+			%CardValue.text = "Q"
 		11:
-			card_value.text = "J"
+			%CardValue.text = "J"
 		_:
-			card_value.text = "%d" % [data.value]
+			%CardValue.text = "%d" % [data.value]
 
 
 ## Show the selection indicator for the card layout.
@@ -50,12 +45,11 @@ func select():
 	#	the bottom of the card, I have no idea why.
 	var global_bottom_center = Vector2(
 		global_position.x + size.x / 2,
-		global_position.y + size.y / 2 + selected_indicator_size.y,
+		global_position.y + size.y + selected_indicator_size.y / 2,
 	)
 
 	Globals.selected_indicator.show()
 	Globals.selected_indicator.position = global_bottom_center
-
 
 
 ## Hide the selection indicator for the card layout.
